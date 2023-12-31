afe1cb1601ccf8b3364d8ce841085fdb8632d7a6
Jimmy Hogerty <jhogerty@housecanary.com>
2023-12-31 11:23:57 -0800

    Added S3 "get" and "post" img storage

--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -4,9 +4,6 @@ import Image from "next/image";
 import classes from "./meal-item.module.css";
 
 export default function MealItem({ title, slug, image, summary, creator }) {
-  console.log(
-    `https://jimmy-nextjs-demo-users-image.s3.amazonaws.com/${image}`
-  );
   return (
     <article className={classes.meal}>
       <header>
